8d9ecb60b749dde50daa6780db0f15b60a88301e
Laurentiu Stamate <laurentiu.stamate@equilobe.com>
2019-01-06 22:57:01 +0100

    Add constants file

---
+++ app/src/main/java/mobile/computing/laurentiu/timetableapp/Constants.java
@@ -0,0 +1,15 @@
+package mobile.computing.laurentiu.timetableapp;
+
+public class Constants {
+    public static final String BACHELOR_SCHEDULE_PATTERN = "^([0-9]?)([0-9]?)([0-9]?)([a-zA-Z]?)([a-zA-Z]?)(a|b|l|r|s|d|p|i|par|impar|odd|even)$";
+
+    public static final String MASTER_SCHEDULE_PATTERN = "^([0-9]?)([0-9]?)([a-zA-Z]*)?(a|b|l|r|s|d|p|i|par|impar|odd|even)$";
+
+    public static final long ONE_MEGABYTE = 1024 * 1024;
+
+    public static final int NUMBER_OF_DAYS_IN_WEEK = 7;
+
+    public static final int NUMBER_OF_DAYS_IN_YEAR = 366;
+
+    public static final String FIREBASE_BASE_URL = "<<< ADD YOUR OWN FIREBASE URL HERE >>>";
+}
